72a94f9a69533e1fc75da0431585f2d77ec5e328
yeong <chlgkdud111@gmail.com>
2025-01-07 15:28:10 +0900

    Add : 관광지 엔티티, 레파지토리 만듦.

---
+++ src/main/java/com/helloseoul/domain/TouristSpot.java
@@ -0,0 +1,33 @@
+package com.helloseoul.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class TouristSpot {
+	
+	@Id
+	private Long postSn; // 고유번호
+	private String lanCodeId; // 언어코드
+	private String postSj; // 관광지 이름
+	private String address; // 주소(지번주소)
+	private String newAddress; // 주소(도로명주소)
+	private String tel; // 전화번호
+	private String url; // 홈페이지 URL
+	private String time; // 운영시간
+	private String day; // 운영요일
+	private String restDay; // 휴무일
+	private String subwayInfo; // 오시는길(교통정보)
+	private String tag; // 태그
+	private String BF_DESC; // 장애인 편의시설
+	private String img; // 이미지
+	
+
+}
---
+++ src/main/java/com/helloseoul/repository/TouristSpotRepository.java
@@ -0,0 +1,11 @@
+package com.helloseoul.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.helloseoul.domain.TouristSpot;
+
+@Repository
+public interface TouristSpotRepository extends JpaRepository<TouristSpot, Long>{
+
+}
