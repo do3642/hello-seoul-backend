ea14da0602a97c6c812cbdb51244e457d29f4a02
yeong <chlgkdud111@gmail.com>
2025-01-20 20:30:02 +0900

    Fixed : 버그 수정

--- src/main/java/com/helloseoul/service/TouristSpotService.java
+++ src/main/java/com/helloseoul/service/TouristSpotService.java
@@ -131,18 +131,6 @@ public class TouristSpotService {
 	public List<TouristSpot> getTouristSpot(String languageCode) {
 		return touristSpotRepository.findByLanguageCodeAndContentTypeId(languageCode);
 	}
-	
-	public Page<TouristSpot> searchTouristSpots(String keyword, int page, int size) {
-		// 검색 키워드가 없으면 기본적으로 모든 관광지를 반환
-		String searchKeyword = keyword == null || keyword.trim().isEmpty() ? "" : keyword ;
-		
-		// 페이징 객체 생성
-		Pageable pageable = PageRequest.of(page, size);
-		
-		// 검색 조건에 맞는 관광지 목록 반환
-		return touristSpotRepository.findByTitleContainingOrGuNameContaining(searchKeyword, pageable);
-		
-	}
 
 	public void fetchAndSaveTouristDateDetails() {
 		System.out.println("1. TouristSpot 데이터 가져오기 시작...");
